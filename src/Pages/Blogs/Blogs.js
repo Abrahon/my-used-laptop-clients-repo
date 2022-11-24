e7b35983a1fc1493d744b314e0cb2b41312c03eb
abrahon <mdabrahon926@gmail.com>
2022-11-24 14:35:45 +0600

    added question-3

--- src/Pages/Blogs/Blogs.js
+++ src/Pages/Blogs/Blogs.js
@@ -15,7 +15,11 @@ const Blogs = () => {
             <h1 className='text-2xl mb-5 font-bold text-warning'>2. How does prototypical inheritance work?</h1>
             <p className='text-xl mb-5'>Prototypical inheritance allows us to reuse the properties or methods from one JavaScript object to another through a reference pointer function. All JavaScript objects inherit properties and methods from a prototype: Date objects inherit from Date.</p>
 
-            
+            <h1 className='text-2xl mb-5 font-bold text-warning'>3. What is a unit test? Why should we write unit tests?</h1>
+            <p className='text-xl mb-5'>A unit test is a way of testing a unit - the smallest piece of code that can be logically isolated in a system. In most programming languages, that is a function, a subroutine, a method or property.<br />
+                For Test-Driven Development (TDD), you write unit tests before writing any implementation. This makes your implementation details in your code shorter and easier to understand. In this instance, the best time to write unit tests is immediately. For others, most developers write unit tests after the code's been written.
+            </p>
+           
         </div>
     );
 };
