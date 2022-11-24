56bf715d545aa45bd9f9b6a3a708d830deea9d08
abrahon <mdabrahon926@gmail.com>
2022-11-24 14:34:36 +0600

    added question-2

--- src/Pages/Blogs/Blogs.js
+++ src/Pages/Blogs/Blogs.js
@@ -12,7 +12,10 @@ const Blogs = () => {
                     4.URL state
                 </strong>
             </p>
-           
+            <h1 className='text-2xl mb-5 font-bold text-warning'>2. How does prototypical inheritance work?</h1>
+            <p className='text-xl mb-5'>Prototypical inheritance allows us to reuse the properties or methods from one JavaScript object to another through a reference pointer function. All JavaScript objects inherit properties and methods from a prototype: Date objects inherit from Date.</p>
+
+            
         </div>
     );
 };
